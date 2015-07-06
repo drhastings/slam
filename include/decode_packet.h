314584d01d285a23d358f49f7316d4b773455381
= <=>
2015-07-06 02:41:18 +0000

    as

--- include/decode_packet.h
+++ include/decode_packet.h
@@ -4,5 +4,5 @@
 #include "matrices.h"
 
 int decode_packet(int16_t ranges[360], double readings[360 * 2], 
-          struct mat *** map, char packet[22]);
+          struct mat ** map, char packet[22]);
 
